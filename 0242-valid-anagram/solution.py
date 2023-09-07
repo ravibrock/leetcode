f2604e947e7aeaddee98bfaeaa2885ed9086f67d
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-07 17:39:09 +0000

    [LC Sync] Runtime - 55 ms (53.52%), Memory - 17.5 MB (9.97%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
+        length = len(s)
+        if length != len(t): return False
         s, t = sorted(s), sorted(t)
 
-        for i in range(len(s)):
+        for i in range(length):
             if s[i] != t[i]: return False
 
         return True
