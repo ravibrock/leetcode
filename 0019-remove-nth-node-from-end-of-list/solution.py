8c66b69b23a472746d3fa9b61bb25bba17143ae2
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-11 18:37:33 +0000

    [LC Sync] Runtime - 49 ms (6.24%), Memory - 16.3 MB (17.81%)

--- 0019-remove-nth-node-from-end-of-list/solution.py
+++ 0019-remove-nth-node-from-end-of-list/solution.py
@@ -7,12 +7,13 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         target, end = head, head
 
-        while end:
-            if n < 0: target = target.next
+        for _ in range(n): end = end.next
+        if not end: return head.next
+
+        while end.next:
+            target = target.next
             end = end.next
-            n -= 1
         
-        if n == 0: return head.next
-        target.next = target.next.next if target.next else target.next
+        target.next = target.next.next
 
         return head
