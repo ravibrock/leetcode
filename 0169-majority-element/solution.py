ba9617a78daaa6d17172557872cb22a0f8434cd1
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-13 14:41:27 +0000

    [LC Sync] Runtime - 155 ms (37.38%), Memory - 17.8 MB (81.92%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -2,6 +2,4 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n, x, y = Counter(nums), 0, 0
         for i, j in n.items():
-            if j > x: x, y = j, i
-
-        return y
+            if j > len(nums) / 2: return i
