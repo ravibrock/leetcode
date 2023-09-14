adabe8d0bafe7926acd9de4d3c3c83ff031f8bbc
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-14 12:44:52 +0000

    [LC Sync] Runtime - 46 ms (7.90%), Memory - 16.1 MB (78.96%)

--- 0908-middle-of-the-linked-list/solution.py
+++ 0908-middle-of-the-linked-list/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head.next: return head
         slow, fast = head, head
         while fast is not None and fast.next is not None:
             fast = fast.next.next
