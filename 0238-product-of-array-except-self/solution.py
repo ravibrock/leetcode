9abc547cf712a23911ceb5373d5297a19fb2c2df
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-21 11:30:55 +0000

    [LC Sync] Runtime - 211 ms (14.26%), Memory - 24.6 MB (30.56%)

--- 0238-product-of-array-except-self/solution.py
+++ 0238-product-of-array-except-self/solution.py
@@ -1,12 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         length = len(nums)
-        sol = [1] * length
-        pre = post = 1
-        for i in range(length):
-            sol[i] *= pre
-            pre = pre*nums[i]
-            sol[length - i - 1] *= post
-            post = post * nums[length - i - 1]
-        
-        return sol
+        pre, post = [1] * length, [1] * length
+
+        for i in range(1, length):
+            pre[i] = nums[i - 1] * pre[i - 1]
+            post[length - i - 1] = nums[length - i] * post[length - i]
+
+        return [pre[i] * post[i] for i in range(len(pre))]
