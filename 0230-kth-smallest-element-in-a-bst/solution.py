29b95a3f902e2d2aa60e28b212843f71cec7d56c
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-28 13:03:49 +0000

    [LC Sync] Runtime - 57 ms (34.64%), Memory - 20.3 MB (70.92%)

--- 0230-kth-smallest-element-in-a-bst/solution.py
+++ 0230-kth-smallest-element-in-a-bst/solution.py
@@ -1,9 +1,3 @@
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        return self.DFS(root)[k - 1]
-    
-    def DFS(self, root, vals=[]):
-        if root:
-            return self.DFS(root.left, vals) + [root.val] + self.DFS(root.right, vals)
-        else:
-            return vals
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int: return self.DFS(root)[k - 1]
+    def DFS(self, r, v=[]): return self.DFS(r.left, v) + [r.val] + self.DFS(r.right, v) if r else v
