ae25d44c2b056885b193332b3cf689fbfff0edec
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-28 13:04:00 +0000

    [LC Sync] Runtime - 121 ms (39.33%), Memory - 18.6 MB (24.55%)

--- 0042-trapping-rain-water/solution.py
+++ 0042-trapping-rain-water/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        formax, backmax = [height[0]], [height[-1]]
+        formax, backmax = height.copy(), height.copy()
         for i in range(1, len(height)):
-            formax.append(formax[i - 1] if formax[i - 1] > height[i] else height[i])
-            backmax.append(backmax[i - 1] if backmax[i - 1] > height[-(i + 1)] else height[-(i + 1)])
-        backmax = list(reversed(backmax))
+            formax[i] = formax[i - 1] if formax[i - 1] > height[i] else height[i]
+            backmax[-(i + 1)] = backmax[-i] if backmax[-i] > height[-(i + 1)] else height[-(i + 1)]
 
         solution = []
         for i in range(1, len(height) - 1):
