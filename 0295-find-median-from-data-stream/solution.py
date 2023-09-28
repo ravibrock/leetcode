3aded7353eb5c384a4583f9aa9646d35bb34b701
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-09-28 13:04:11 +0000

    [LC Sync] Runtime - 1317 ms (11.39%), Memory - 37.9 MB (85.99%)

--- 0295-find-median-from-data-stream/solution.py
+++ 0295-find-median-from-data-stream/solution.py
@@ -3,17 +3,35 @@ class MedianFinder:
         self.vals = []
 
     def addNum(self, num: int) -> None:
-        self.vals.append(num)
-
+        length = len(self.vals)
+        if length == 0 or self.vals[-1] <= num:
+            self.vals.append(num)
+            return
+        
+        if num <= self.vals[0]:
+            self.vals.insert(0, num)
+            return
+        
+        if len(self.vals) == 2:
+            self.vals.insert(1, num)
+            return
+        
+        l, r = 0, length - 1
+        while l <= r:
+            mid = (r + l) // 2
+            if self.vals[mid] <= num < self.vals[mid + 1]:
+                self.vals.insert(mid + 1, num)
+                return
+            if self.vals[mid] < num: l = mid + 1
+            else: r = mid - 1
+        
+        self.vals.insert(mid + 1, num)
+    
     def findMedian(self) -> float:
-        self.vals = sorted(self.vals)
-        mid1 = (len(self.vals) - 1) // 2
-        if len(self.vals) % 2 == 0:
-           print(float((self.vals[mid1] + self.vals[mid1 + 1])) / 2)
-           return float((self.vals[mid1] + self.vals[mid1 + 1])) / 2
-        else:
-            print(self.vals[mid1])
-            return self.vals[mid1]
+        mid = (len(self.vals) - 1) // 2
+        if len(self.vals) % 2 == 0: return float((self.vals[mid] + self.vals[mid + 1])) / 2
+        else: return self.vals[mid]
+           
 
 
 # Your MedianFinder object will be instantiated and called as such:
